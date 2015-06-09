13e7347d2a65d6ada0c89fa56301c6d3df523a58
DET-Labor Rechner <christian.roemer@tu-dortmund.de>
2015-06-09 17:03:32 +0200

    init commit

---
+++ desktop/src/de/det/d3t/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.det.d3t.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.det.d3t.MainFrame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainFrame(), config);
+	}
+}
