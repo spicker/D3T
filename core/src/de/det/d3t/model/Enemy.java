753baeac01f07165ca7b854e5f8b9fc66a7ad03a
Tobias Klöppner <tobias.kloeppner@live.de>
2015-06-19 21:14:22 +0200

    removed strange import

--- core/src/de/det/d3t/model/Enemy.java
+++ core/src/de/det/d3t/model/Enemy.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.sun.xml.internal.messaging.saaj.soap.impl.DetailImpl;
 
 import de.det.d3t.Settings;
 import de.det.d3t.TextureFactory;
