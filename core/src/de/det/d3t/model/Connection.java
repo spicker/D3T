797e2a171b4a5231905079b4fc205571ac7da292
Tobias.Kloeppner <tobias.kloeppner@tu-dortmund.de>
2015-06-30 17:18:52 +0200

    work on line segment physics

--- core/src/de/det/d3t/model/Connection.java
+++ core/src/de/det/d3t/model/Connection.java
@@ -49,18 +49,23 @@ public class Connection extends Actor {
 		angle = (float) Math.atan2(difY, difX);
 		angle = (float) Math.toDegrees(angle);
 		int c = 0;
-		for(float lx = 0; lx < distance; lx += step){
-			Sprite s = new Sprite(t);
+		for (float lx = 0; lx < distance; lx += step) {
+			Sprite s = new Sprite(texture);
 			s.setU2(0);
 			s.setV2(0);
 			s.setV(1f);
 			s.setU(1);
-			s.setBounds(x1 + (step * difX) * c, y1 + (step * difY) * c - width / 2, step, width);
+			s.setBounds(x1 + (step * difX) * c, y1 + (step * difY) * c - width
+					/ 2, step, width);
 			s.setOrigin(0, width / 2f);
 			s.setRotation(angle);
 			segments.add(s);
 			c++;
-		}
+		} 
+	}
+	
+	public float getDistance() {
+		return distance;
 	}
 	
 	public void setAnkers(float x1, float y1, float x2, float y2) {
@@ -103,38 +108,39 @@ public class Connection extends Actor {
 				first = false;
 			}
 		}
-		Sprite s = segments.get(segments.size() - 1);
-		float stx = s.getX() + step * difX;
-		float sty = s.getY() + step * difY;
-		stx -= x2;
-		sty -= y2;
-		float distance =  (float) Math.sqrt(stx * stx + sty * sty);
-		if(distance > step){
-			segments.remove(s);
-		}else{
-			s.setU2(1 - (distance / step));
-			s.setSize(step - distance, width);
+		if(segments.size() > 0){
+			Sprite s = segments.get(segments.size() - 1);
+			float stx = s.getX() + step * difX;
+			float sty = s.getY() + step * difY;
+			stx -= x2;
+			sty -= y2;
+			float distance =  (float) Math.sqrt(stx * stx + sty * sty);
+			if(distance > step && segments.size() > this.distance / step){
+				//segments.remove(s);
+			}else{
+				s.setU2(1 - (distance / step));
+				s.setSize(step - distance, width);
+			}
+			
+			if(segments.size() >= 2){
+				s = segments.get(0);
+				Sprite s2 = segments.get(1);
+				stx = s2.getX() - x1;
+				sty = s2.getY() - y1;
+				distance =  (float) Math.sqrt(stx * stx + sty * sty);
+				if(distance > step){
+					s.setSize(step + 1, width);
+					Sprite s3 = new Sprite(texture);
+					s3.setBounds(x1, y1, distance - step, width);
+					s3.setOrigin(0, width / 2f);
+					s3.setRotation(angle);
+					segments.add(0, s3);
+				}else{
+					s.setU(1 - (distance / step));
+					s.setSize(step - (step - distance), width);
+				}
+			}
 		}
-		
-		
-		s = segments.get(0);
-		Sprite s2 = segments.get(1);
-		stx = s2.getX() - x1;
-		sty = s2.getY() - y1;
-		distance =  (float) Math.sqrt(stx * stx + sty * sty);
-		if(distance > step){
-			s.setSize(step + 1, width);
-			Sprite s3 = new Sprite(texture);
-			s3.setBounds(x1, y1, distance - step, width);
-			s3.setOrigin(0, width / 2f);
-			s3.setRotation(angle);
-			segments.add(0, s3);
-		}else{
-			s.setU(1 - (distance / step));
-			s.setSize(step - (step - distance), width);
-		}
-		
-		
 	}
 	
 	@Override
